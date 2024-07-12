74244064c9ab1739114b278bfc7d5a27640e6d9f
parkjh <ace2006jh@naver.com>
2024-07-12 10:20:41 +0900

    FAQ 디자인

--- backend/mybuild.sh
+++ backend/mybuild.sh
@@ -12,18 +12,18 @@ cp -r ../frontend/public src/main/resources/static
 ./gradlew bootJar
 
 # build image
-docker build -t jeonghyeon123/alba-connector .
+docker build -t xenoble/alba-connector .
 
 # push image
-docker push jeonghyeon123/alba-connector
+docker push xenoble/alba-connector
 
 # remote 에서
 
 # 컨테이너 멈추고
-ssh -i src/main/resources/secret/key0527.pem ubuntu@43.201.25.153 'docker stop alba-connector'
+ssh -i src/main/resources/secret/keyy.pem ubuntu@3.34.45.35 'docker stop alba-connector'
 # 컨테이너 삭제
-ssh -i src/main/resources/secret/key0527.pem ubuntu@43.201.25.153 'docker rm alba-connector'
+ssh -i src/main/resources/secret/keyy.pem ubuntu@3.34.45.35 'docker rm alba-connector'
 # pull image
-ssh -i src/main/resources/secret/key0527.pem ubuntu@43.201.25.153 'docker pull jeonghyeon123/alba-connector'
+ssh -i src/main/resources/secret/keyy.pem ubuntu@3.34.45.35 'docker pull xenoble/alba-connector'
 # 컨테이너 실행
-ssh -i src/main/resources/secret/key0527.pem ubuntu@43.201.25.153 'docker run -d -p 8080:8080 --restart always --name alba-connector jeonghyeon123/alba-connector'
+ssh -i src/main/resources/secret/keyy.pem ubuntu@3.34.45.35 'docker run -d -p 8080:8080 --restart always --name alba-connector xenoble/alba-connector'
